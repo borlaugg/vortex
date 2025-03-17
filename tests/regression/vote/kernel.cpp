da96578623225da674d9ad223c460cae6627e5d3
Rishabh Ravi <rishabh0926@gmail.com>
2025-03-17 00:37:25 -0400

    Testing kernel/conform

--- tests/regression/vote/kernel.cpp
+++ tests/regression/vote/kernel.cpp
@@ -2,6 +2,50 @@
 #include "common.h"
 #include <vx_intrinsics.h>
 
+volatile int barrier_ctr;
+volatile int barrier_stall;
+int barrier_buffer[8];
+int tls_buffer[8];
+__thread int tls_var;
+
+int __attribute__((noinline)) check_error(const int* buffer, int offset, int size) {
+	int errors = 0;
+	for (int i = offset; i < size; i++)	{
+		int value = buffer[i];
+		int ref_value = 65 + i;
+		if (value == ref_value)	{
+			//PRINTF("[%d] %c\n", i, value);
+		} else {
+			vx_printf("FAIL");
+			++errors;
+		}
+	}
+	return errors;
+}
+
+__attribute__((noinline)) void print_tls_var() {
+	unsigned wid = vx_warp_id();
+	tls_buffer[wid] = 65 + tls_var;
+}
+
+void tls_kernel() {
+	unsigned wid = vx_warp_id();
+	tls_var = wid;
+	print_tls_var();
+	vx_tmc(0 == wid);
+}
+
+void barrier_kernel() {
+	unsigned wid = vx_warp_id();
+	for (int i = 0; i <= (wid * 256); ++i) {
+		++barrier_stall;
+	}
+	barrier_buffer[wid] = 65 + wid;
+	vx_barrier(0, barrier_ctr);
+	vx_tmc(0 == wid);
+}
+
+
 void kernel_body(kernel_arg_t* __UNIFORM__ arg) {
 	uint32_t count    = arg->task_size;
 	int32_t* src0_ptr = (int32_t*)arg->src0_addr;
@@ -23,6 +67,18 @@ void kernel_body(kernel_arg_t* __UNIFORM__ arg) {
     
     // int vote_all = vx_vote_sync(0, 0, -1, val);   
 	vx_vote();
+
+	int num_warps = (vx_num_warps()< 8)?vx_num_warps():8;
+	barrier_ctr = num_warps;
+	barrier_stall = 0;
+	vx_wspawn(num_warps, barrier_kernel);
+	barrier_kernel();
+	int error = check_error(barrier_buffer, 0, num_warps);
+
+	num_warps = (vx_num_warps()< 8)?vx_num_warps():8;
+	vx_wspawn(num_warps, tls_kernel);
+	tls_kernel();
+	check_error(tls_buffer, 0, num_warps);
 
 }                
           
